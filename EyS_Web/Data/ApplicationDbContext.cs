21f181cd970f0c123586718f91a3ee82c5bb1cc2
ernesto2546 <ernestonicolas2546@gmail.com>
2024-12-10 14:43:05 -0400

    Initial Commit

---
+++ EyS_Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EyS_Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EyS_Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Sci-Fi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Adventure", DisplayOrder = 4 }
+                );
+        }
+    }
+}
