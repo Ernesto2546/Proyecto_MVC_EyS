16b26c1e4f91c82eb9332b139138692cd9082826
ernesto2546 <ernestonicolas2546@gmail.com>
2024-12-11 15:29:04 -0400

    Add EyS.Models

---
+++ EyS.Models/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace EyS_Web.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; init; }
+        [Required]
+        [MaxLength(40)]
+        [DisplayName("Category Name")]
+        public string Name { get; init; }
+        [DisplayName("Display Order")]
+        [Range(1,150,ErrorMessage ="Display Order must be between 1-150")]
+        public int DisplayOrder { get; init; }
+
+    }
+}
