0c396a9533386971663305b6bc807b532ae53a61
ernesto2546 <ernestonicolas2546@gmail.com>
2024-12-11 08:51:20 -0400

    Create Proyect WebRazor_Temp

--- EyS_Web/obj/Release/net8.0/EyS_Web.AssemblyInfo.cs
+++ EyS_Web/obj/Release/net8.0/EyS_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EyS_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717daf1d6f530711637fd9df745111f7fd222936")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50f0cbe880a2c14c661b1427455370b5c6f4a58")]
 [assembly: System.Reflection.AssemblyProductAttribute("EyS_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EyS_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
