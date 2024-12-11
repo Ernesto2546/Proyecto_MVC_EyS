4aad1754b572b5c3bbfe74cb312d9823a02cd8b9
ernesto2546 <ernestonicolas2546@gmail.com>
2024-12-11 12:02:47 -0400

    Update .gitignore, Create files "EyS.DataAccess" "EyS.Utility" "EyS.Models"

--- EyS_Web/obj/Debug/net8.0/EyS_Web.AssemblyInfo.cs
+++ EyS_Web/obj/Debug/net8.0/EyS_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EyS_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+765bad8e72be41e3230d06c0edda6e6bf3ada11b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f160e9aecceffa7a48226325c4ac40a924828b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EyS_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EyS_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
